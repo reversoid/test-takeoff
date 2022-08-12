52370063b025ad14eed834a6f3b2ed6a73fb0c59
George <george.kobunov@ddgcorp.ru>
2022-08-12 12:01:31 +0300

    implemented registration endpoint

--- src/app/mockServer/server.js
+++ src/app/mockServer/server.js
@@ -4,36 +4,67 @@ const server = jsonServer.create();
 const path = require("path");
 const router = jsonServer.router(path.join(__dirname, "db.json"));
 const middlewares = jsonServer.defaults();
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
-const PRIVATE_KEY = '5a12321312B3S0s4a8v4x479590zzz5ze59t38yXXX';
+const PRIVATE_KEY = "5a12321312B3S0s4a8v4x479590zzz5ze59t38yXXX";
 
 function createJWT(payload) {
-  return jwt.sign(payload, PRIVATE_KEY, {expiresIn: '7d'});
+  return jwt.sign(payload, PRIVATE_KEY, { expiresIn: "7d" });
 }
 
 server.use(middlewares);
 server.use(jsonServer.bodyParser);
 
+server.post("/api/auth/login", async (req, res) => {
+  try {
+    const { login, password } = req.body;
+    if (!(login && password))
+      return res.status(400).json({ message: "Pass all required values" });
+    const response = await axios.get(
+      `http://localhost:4444/api/users?login=${login}`
+    );
+    if (response.data.length === 0)
+      return res
+        .status(400)
+        .json({ message: "Incorrect username or password" });
 
-server.post('/api/auth/login', async (req, res) => {
-  const { login, password } = req.body;
-  if (!(login && password)) return res.status(400).json({message: "Pass all required values"});
-  const response = await axios.get(`http://localhost:4444/api/users?login=${login}`);
-  if (response.data.length === 0) return res.status(400).json({message: "Incorrect username or password"});
+    if (response.data[0].password === password)
+      return res
+        .status(200)
+        .json({ token: createJWT({ login: response.data[0].login }) });
+    return res.status(400).json({ message: "Incorrect username or password" });
+  } catch (error) {
+    return res.status(500).json({ message: "Internal service error", info: error });
+  }
+});
 
-  if (response.data[0].password === password) return res.status(200).json({token: createJWT({login: response.data[0].login})})
-  return res.status(400).json({message: "Incorrect username or password"});
-})
+server.post("/api/auth/registration", async (req, res) => {
+  try {
+    const { name, login, password } = req.body;
+    if (!(login && password && name))
+      return res.status(400).json({ message: "Pass all required values" });
 
-server.post('/api/auth/registration', (req, res) => {
-  res.json(req.body);
-})
+    const response = await axios.get(
+      `http://localhost:4444/api/users?login=${login}`
+    );
+    if (response.data.length)
+      return res.status(400).json({ message: "User already exists" });
 
+    await axios.post(`http://localhost:4444/api/users`, {
+      name,
+      login,
+      password,
+    });
+    return res
+      .status(200)
+      .json({ token: createJWT({ login }) });
+  } catch (error) {
+    return res.status(500).json({ message: "Internal service error", info: error });
+
+  }
+});
 
 server.use("/api", router);
-
-
 
 server.listen(4444, () => {
   console.log("JSON Server is running");
