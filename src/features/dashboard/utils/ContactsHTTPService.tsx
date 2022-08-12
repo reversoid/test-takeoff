bbc857e59fd0b41c9cf2b7ee00243b13457626f7
George <george.kobunov@ddgcorp.ru>
2022-08-12 21:07:58 +0300

    now access headers are added to each contact request

--- src/features/dashboard/utils/ContactsHTTPService.tsx
+++ src/features/dashboard/utils/ContactsHTTPService.tsx
@@ -2,19 +2,23 @@ import axios from "axios";
 import { IContact, IContactDTO } from "./types";
 
 export class ContactsService {
+  private authHeaders =  {
+    token: localStorage.getItem('token') ?? '',
+  }
+
   public getContacts() {
-    return axios.get<IContact[]>("http://localhost:4444/api/contacts", {headers: {token: localStorage.getItem('token') ?? ''}});
+    return axios.get<IContact[]>("http://localhost:4444/api/contacts", {headers: this.authHeaders});
   }
 
   public updateContact(data: { id: number; newData: IContactDTO }) {
-    return axios.put<IContact>(`http://localhost:4444/api/contacts/${data.id}`, data.newData);
+    return axios.put<IContact>(`http://localhost:4444/api/contacts/${data.id}`, data.newData, {headers: this.authHeaders});
   }
 
   public removeContact(id: number) {
-    return axios.delete(`http://localhost:4444/api/contacts/${id}`);
+    return axios.delete(`http://localhost:4444/api/contacts/${id}`, {headers: this.authHeaders});
   }
 
   public addContact(data: IContactDTO) {
-    return axios.post(`http://localhost:4444/api/contacts/`, data);
+    return axios.post(`http://localhost:4444/api/contacts/`, data, {headers: this.authHeaders});
   }
 }
